dac21a91cf38480dd17f5248bd01ba50a687b5ad
Sahilkumarbt <117939412+Sahilkumarbt@users.noreply.github.com>
2025-03-08 18:13:36 +0530

    MONGODB CONNECTED

---
+++ src/index.js
@@ -0,0 +1,41 @@
+// require('dotenv').config({path: './env'})
+import dotenv from 'dotenv';
+dotenv.config({ path: './.env' });
+import connectDB from "./db/index.js";
+
+
+
+// dotenv.config({
+//     path:'/.env'
+// })
+
+connectDB()
+
+
+/*
+import express from "express"
+const app=express()
+(async()=>{
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("ERRR:",error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, ()=>{
+            console.log(`App is listening on port
+                ${process.env.PORT}`);
+        })
+    }catch(error){
+        console.error("ERROR",error);
+        throw error;
+    }
+})()//IIFE
+
+
+  "scripts": {
+    "dev": "nodemon -r dotenv/config--experimental-json-modules src/index.js"
+  },
+*/
+ 
